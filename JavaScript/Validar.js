8008100ba3573f9cdb492191e35fab1bf83fb00c
Miloboty <91978205+Miloboty@users.noreply.github.com>
2021-10-05 14:27:36 -0600

    Add files via upload

---
+++ JavaScript/Validar.js
@@ -0,0 +1,14 @@
+
+
+function validarUsuario(){
+    let user = document.forms["formulario"]["usuario"].value;
+    let psw = document.forms["formulario"]["contraseña"].value;
+    if (user == "diego" && psw == "examen") {
+        sessionStorage.setItem("IngresoCorrecto", "True")
+        return true;
+    }
+
+    sessionStorage.setItem("IngresoCorrecto", "False")
+    alert("Usuario o contraseña incorrecta")
+    return false;
+  }
